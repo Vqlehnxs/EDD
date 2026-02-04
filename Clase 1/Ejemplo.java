2951801264e3295f2a1b3091e0891f35c9be38ff
Stiven <yesid.balanta@campusucc.edu.co>
2026-02-04 20:43:31 +0000

    ejemplo

---
+++ Clase 1/Ejemplo.java
@@ -0,0 +1,14 @@
+public class Ejemplo {
+    public static void main(String[] args) throws Exception {
+        // Creación de arreglo/array/vector
+        int[] a = {2, 6, 8, 1, 20, 40, 7, 3, 5};
+        int suma = 0;
+        System.out.println("Tamaño del arreglo: " + a.length);
+
+        for(int i = 0; i < a.length; i++) {
+            suma += a[i];
+            System.out.println("a[" + i + "]=" + a[i]);
+            System.out.println("Suma: " + suma);
+        }
+    }
+}
