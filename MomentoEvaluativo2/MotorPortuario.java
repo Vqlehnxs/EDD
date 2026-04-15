0af03e11716704ae5187d14da30955fe2cc47516
Stiven <yesid.balanta@campusucc.edu.co>
2026-04-15 22:53:15 +0000

    Documentacion del main

--- MomentoEvaluativo2/MotorPortuario.java
+++ MomentoEvaluativo2/MotorPortuario.java
@@ -6,17 +6,17 @@ public class MotorPortuario {
         System.out.println("║           Puerto de Data-Bay             ║");
         System.out.println("╚══════════════════════════════════════════╝");
 
-        System.out.println("\n Registro de Manifiesto");
+        System.out.println("\n Registro de Manifiesto"); // Este es el Modulo 1
         Manifiesto manifiesto = new Manifiesto(6);
         manifiesto.registrar(new Contenedor("C001", 1200.0, 1));
-        manifiesto.registrar(new Contenedor("C002", 850.5, 3));
-        manifiesto.registrar(new Contenedor("C003", 2300.0, 2));
-        manifiesto.registrar(new Contenedor("C004", 500.0, 4));
+        manifiesto.registrar(new Contenedor("C002", 850.5, 3)); // Prioridad Peligroso
+        manifiesto.registrar(new Contenedor("C003", 2300.0, 2)); 
+        manifiesto.registrar(new Contenedor("C004", 500.0, 4)); // Prioridad Alta
         manifiesto.registrar(new Contenedor("C005", 1800.0, 1));
-        manifiesto.registrar(new Contenedor("C006", 750.0, 3));
+        manifiesto.registrar(new Contenedor("C006", 750.0, 3)); // Prioridad Peligroso
         manifiesto.resumen();
 
-        System.out.println("Patio de Almacenamiento (3x3)");
+        System.out.println("Patio de Almacenamiento (3x3)"); // Este es el Modulo 2
         PatioAlmacenamiento patio = new PatioAlmacenamiento(3, 3);
         try{
             for(Contenedor c: manifiesto.getContenedores()){
@@ -27,7 +27,7 @@ public class MotorPortuario {
         }
         patio.mostarPatio();
 
-        System.out.println("Bahia de Inspeccion");
+        System.out.println("Bahia de Inspeccion"); // Este es el Modulo 3
         BahiaInspeccion inspeccion = new BahiaInspeccion();
         for(Contenedor c : manifiesto.getContenedores()){
             inspeccion.encolar(c);
@@ -38,21 +38,26 @@ public class MotorPortuario {
             inspeccion.inspeccionar();
         }
 
-        System.out.println("Ubicacion en el Buque");
-        Buque buque = new Buque(true);
+        System.out.println("Ubicacion en el Buque"); // Este es el Modulo 4
+        Buque buque = new Buque(true); // El modo de estabilidad estara en true
 
         Contenedor[] carga = {
             new Contenedor("C003", 2300.0, 2),
             new Contenedor("C001", 1200.0, 1),
-            new Contenedor("C005", 1800.0, 1),
+            new Contenedor("C005", 1800.0, 1), // Sera rechazado al ser mayor que 1200
             new Contenedor("C006", 750.0, 3),
-            new Contenedor("C002", 850.0, 3),
+            new Contenedor("C002", 850.0, 3), //Sera rechazado al ser mayor que 750
             new Contenedor("C004", 500.0, 4),
         };
 
         for(Contenedor c : carga){
             buque.apilar(c);
         }
+        buque.mostrarBuque();
+
+        //Aqui se retira los contenedores afectados del fondo
+        System.out.println("  OPERACIÓN CRITICA: Contenedor del fondo presenta falla..."); 
+        buque.retirarFondo(); 
         buque.mostrarBuque();
 
         System.out.println("╔══════════════════════════════════════════╗");
