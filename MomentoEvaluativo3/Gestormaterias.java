1cce17a6a48748ee86cc7f4ddf6b548bb164c201
Stiven <yesid.balanta@campusucc.edu.co>
2026-05-15 20:53:53 +0000

    Gestor de materias

---
+++ MomentoEvaluativo3/Gestormaterias.java
@@ -0,0 +1,136 @@
+import java.util.HashMap;
+
+public class Gestormaterias {
+    //HashMap para buscar materias rapido por codigo
+    private HashMap<String, Materia> materias;
+    private Gestorestudiantes gestorEstudiantes;
+
+    public Gestormaterias(Gestorestudiantes gestorEstudiantes) {
+        this.materias = new HashMap<>();
+        this.gestorEstudiantes = gestorEstudiantes;
+    }
+
+    public void crearMateria(String codigo, String nombre, int cuposMaximos, int creditos){
+        Materia m = new Materia(codigo, nombre, cuposMaximos, creditos);
+        materias.put(codigo, m);
+        System.out.println("Materia creada: " + m);
+    }
+
+    public Materia buscarMateria(String codigo){
+        return materias.get(codigo);
+    }
+
+    public void agregarPreRequisito(String codigoMateria, String codigoPreReq){
+        Materia m = materias.get(codigoMateria);
+        if(m == null){
+            System.out.println("Error: Materia " + codigoMateria + " no encontrada.");
+            return;
+        }
+        m.agregarPreRequisitos(codigoPreReq);
+        System.out.println("PreRequisito " + codigoPreReq + " agregado a " + codigoMateria);
+    }
+
+    public void mostrarPreRequisitos(String codigoMateria){
+        Materia m = materias.get(codigoMateria);
+        if(m == null){
+            System.out.println("Materia no encontrada");
+            return;
+        }
+        System.out.println("PreRequisitos de "+ codigoMateria + ":" );
+        if(m.getPreRequisitos().isEmpty()){
+            System.out.println(" (Sin PreRequisitos)");
+        } else {
+            for(String req : m.getPreRequisitos()){
+                System.out.println(" -" + req);
+            }
+        }
+    }
+
+    // Ahora un metodo que inscriba un estuidante a una materia, ademas de verificar los prerequisitos y si hay cupos
+    public void inscribirEstudiante(String idEstudiante, String codigoMateria)throws PreRequisitoNoAprobadoException, EstudianteNoEncontradoException{
+        Estudiante estudiante = gestorEstudiantes.buscarPorId(idEstudiante);
+        Materia materia = materias.get(codigoMateria);
+
+        if(materia == null){
+            System.out.println("Error: Materia " + codigoMateria + " no encontrada.");
+            return;
+        }
+
+        //En esta parte verificamos los prerequisitos, mirando el historial del estudiante
+        for(String req : materia.getPreRequisitos()){
+            if(!estudiante.getHistorialMaterias().contains(req)){
+                throw new PreRequisitoNoAprobadoException("PreRequisito excepcion - El estudiante no ha cursado: " + req);
+            }
+        }
+
+        if(materia.estaInscrito(idEstudiante)){
+            System.out.println("El estudiante ya se encuentra inscrito en " + codigoMateria);
+            return;
+        }
+
+        if(materia.hayCupo()){
+            System.out.println("Inscripcion exitosa: " + idEstudiante + " en " + codigoMateria);
+        } else{
+            System.out.println("Materia llena. " + idEstudiante + " agrendando a COLA DE ESPERA de " + codigoMateria);
+        }
+    }
+
+    //Ahora debemos de poder cancelar la inscripcion de un estudiante en una materia, asi que retornaremos el id del siguiente en la cola (o null)
+    public String cancelarInscripcion(String idEstuidante, String codigoMateria){
+        Materia materia = materias.get(codigoMateria);
+        if(materia == null){
+            System.out.println("Materia no encontrada.");
+            return null;
+        }
+
+        String siguiente = materia.cancelarInscripcion(idEstuidante);
+        System.out.println("Caneclacion exitosa. Cupo liberado");
+
+        if(siguiente != null){
+            System.out.println("Asignado cupo a " + siguiente + " (primer estuidante en la cola)");
+
+        }
+        return siguiente;
+    }
+
+    public void mostrarColaEspera(String codigoMateria){
+        Materia m = materias.get(codigoMateria);
+        if(m == null){
+            System.out.println("Materia no encontrada");
+            return;
+        }
+
+        System.out.println("--- COLA DE ESPERA ---");
+        System.out.println("Materia: " + m.getCodigo() + " - " + m.getNombre());
+        System.out.println("Cupos totales: " + m.getCuposMaximos());
+        
+        if(m.getColaEspera().isEmpty()){
+            System.out.println("Cola de espera vacia.");
+            return;
+        }
+        int pos = 1;
+        for(String id : m.getColaEspera()){
+            System.out.println("Posicion" + pos + ": " + id);
+        }
+        System.out.println("Total en espera: " + m.getColaEspera().size());
+    }
+
+    public void listarMaterias(){
+        if(materias.isEmpty()){
+            System.out.println("No hay materias registradas");
+            return;
+        }
+        System.out.println("=== MATERIAS ===");
+        for(Materia m : materias.values()){
+            System.out.println(" " + m);
+        }
+    }
+
+    public HashMap<String, Materia> getMaterias() {
+        return materias;
+    }
+
+    
+}
+
+
